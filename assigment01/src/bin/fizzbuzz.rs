2e86307ea35ee52cf215b651f1f7fa0a0d39b5fc
guilloboi1917 <noah.isaak@uzh.ch>
2025-09-29 19:56:49 +0200

    Finish Fizzbuzz

--- assigment01/src/bin/fizzbuzz.rs
+++ assigment01/src/bin/fizzbuzz.rs
@@ -1,55 +1,115 @@
-use core::num;
 use std::io;
 
-fn main() {
-    println!(
-        "Please enter four positive integer numbers (m n x y) separated by one or more blank spaces or type quit."
-    );
-
-    let mut input = String::new();
-
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read input!");
-
-    println!("{}", { validate_input(input) })
+// Input params for fizzbuzz
+struct FizzbuzzParams {
+    m: u32,
+    n: u32,
+    x: u32,
+    y: u32,
 }
 
-// Validate user input
-fn validate_input(user_input: String) -> bool {
+fn main() {
+    loop {
+        println!(
+            "Please enter four positive integer numbers (m n x y) separated by one or more blank spaces or type quit."
+        );
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input!");
+
+        // Match parsed input to some actions
+        match parse_input(input) {
+            // QUIT
+            Ok(None) => break,
+
+            // VALID INPUT
+            Ok(Some(params)) => {
+                println!("{}", run_fizzbuzz(params.m, params.n, params.x, params.y));
+                continue;
+            }
+
+            // ERROR
+            Err(err_msg) => {
+                println!("Err {}", err_msg);
+                continue;
+            }
+        }
+    }
+}
+
+fn run_fizzbuzz(m: u32, n: u32, x: u32, y: u32) -> String {
+    let mut result = String::new();
+
+    // Iterate over all values
+    for i in m..=n {
+        // Check fizzbuzz rules
+        if i % (x * y) == 0 {
+            result.push_str("FizzBuzz");
+        } else if i % x == 0 {
+            result.push_str("Fizz");
+        } else if i % y == 0 {
+            result.push_str("Buzz");
+        } else {
+            result.push_str(&i.to_string());
+        }
+
+        // Add space between elements (except last one)
+        if i < n {
+            result.push(' ');
+        }
+    }
+
+    result
+}
+
+// Validate user input by returning a result
+// Quit -> Ok(None)
+// Run fizzbuzz -> Ok(params)
+// Error -> Err
+fn parse_input(user_input: String) -> Result<Option<FizzbuzzParams>, String> {
+    // Trim input and check for quit statement
     let trimmed_input = user_input.trim();
 
-    if trimmed_input == "quit" {
-        return true;
+    if trimmed_input.eq_ignore_ascii_case("quit") {
+        return Ok(None);
     }
 
-    // Split string
-    let input_iter = user_input.split_ascii_whitespace();
-
-    let input_list: Vec<&str> = input_iter.collect();
+    // Collect into str-vector
+    let input_list: Vec<&str> = trimmed_input.split_ascii_whitespace().collect();
 
     if input_list.len() != 4 {
-        return false;
+        return Err("Please enter exactly 4 numbers".to_string());
     }
 
-    let mut input_array: [i32; 4] = [0, 0, 0, 0];
+    let mut params = [0u32; 4];
 
     for (i, param) in input_list.iter().enumerate() {
-        let number: i32 = param.parse().expect("Not a valid input");
-        if number < 0 {
-            return false;
-        };
-        input_array[i] = number;
+        // Parse to a unsigned integer and will get an error if a number < 1 will be provided
+        // Using map_err for more concise code, could use match result and return Ok and Err
+        params[i] = param
+            .parse()
+            .map_err(|_| format!("'{}' is not a positive integer", param))?;
+
+        if params[i] == 0 {
+            return Err("Number must be positive (> 0)".to_string());
+        }
     }
+
+    // Get parameters
+    let [m, n, x, y] = params;
 
     // final check on numbers
-    if input_array[1] <= input_array[0] {
-        return false;
+    if n <= m {
+        return Err("n must be larger than m".to_string());
     }
 
-    if input_array[2] == input_array[3] {
-        return false;
+    if x == y {
+        return Err("x must be different than y".to_string());
     }
 
-    return true;
+    // We return Ok with the params attached
+    return Ok(Some(FizzbuzzParams { m, n, x, y }));
 }
